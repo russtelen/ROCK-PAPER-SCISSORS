7f55d1e590012b904026c448565015de2054f970
Russell Telen <rtelen@my.bcit.ca>
2021-01-14 13:51:29 -0800

    Create project files

---
+++ RockPaperScissors/RockPaperScissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Russell Telen on 2021-01-14.
+//  Copyright © 2021 Russ Telen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

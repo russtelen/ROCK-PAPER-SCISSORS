c3fe7cfd79d7891e520f2a0be1d62ed06f35dad0
Russell Telen <rtelen@my.bcit.ca>
2021-01-14 14:23:08 -0800

    Connect UIs to ViewController.swift

--- RockPaperScissors/RockPaperScissors/ViewController.swift
+++ RockPaperScissors/RockPaperScissors/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var p1item: UIImageView!
+  @IBOutlet weak var p2item: UIImageView!
+  @IBOutlet weak var result: UILabel!
+  
+  @IBAction func p1ButtonPressed(_ sender: UIButton) {
+    print("p1 button pressed")
+  }
+  
+  
+  @IBAction func p2ButtonPressed(_ sender: UIButton) {
+   print("p2 button pressed")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.

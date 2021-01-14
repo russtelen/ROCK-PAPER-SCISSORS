2661d1b3b4005fff40069ff47b37443b48d61832
Russell Telen <rtelen@my.bcit.ca>
2021-01-14 15:08:07 -0800

    Set up initial logic

--- RockPaperScissors/RockPaperScissors/ViewController.swift
+++ RockPaperScissors/RockPaperScissors/ViewController.swift
@@ -14,20 +14,52 @@ class ViewController: UIViewController {
   @IBOutlet weak var p2item: UIImageView!
   @IBOutlet weak var result: UILabel!
   
+  // array of rock, paper, scissors
+  let itemArray: [UIImage] = [(UIImage(named: "paper")!),(UIImage(named: "rock")!),(UIImage(named: "scissors")!)]
+  
+  // p1 button
   @IBAction func p1ButtonPressed(_ sender: UIButton) {
-    print("p1 button pressed")
+    // randomize item(rock, paper scissors)
+    if let randomItem = itemArray.randomElement() {
+      p1item.image = randomItem
+    }
+    
+    // update result
+    updateResult()
   }
   
-  
+  // p2 button
   @IBAction func p2ButtonPressed(_ sender: UIButton) {
-   print("p2 button pressed")
+    // randomize item(rock, paper scissors)
+   if let randomItem = itemArray.randomElement() {
+     p2item.image = randomItem
+   }
+    
+    // update result
+    updateResult()
+  }
+  
+  // update results
+  func updateResult() {
+    if
+       (p1item.image! == (UIImage(named: "paper")) && p2item.image! == (UIImage(named: "rock"))) ||
+       (p1item.image! == (UIImage(named: "rock")) && p2item.image! == (UIImage(named: "scissors"))) ||
+       (p1item.image! == (UIImage(named: "scissors")) && p2item.image! == (UIImage(named: "paper"))) {
+         result.text = "Player 1 Wins!"
+    } else if
+      (p2item.image! == (UIImage(named: "paper")) && p1item.image! == (UIImage(named: "rock"))) ||
+      (p2item.image! == (UIImage(named: "rock")) && p1item.image! == (UIImage(named: "scissors"))) ||
+      (p2item.image! == (UIImage(named: "scissors")) && p1item.image! == (UIImage(named: "paper"))) {
+         result.text = "Player 2 Wins!"
+    } else {
+         result.text = "Its a tie!"
+    }
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
-
 }
+
 
